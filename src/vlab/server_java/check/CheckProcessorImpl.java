cee351e3e2bf934ee5f2b6be193a84cf276ade3b
mprosolovich <mih.prosolovich@gmail.com>
2022-04-21 13:01:27 +0300

    fail-fast checking logic implemented

--- src/vlab/server_java/check/CheckProcessorImpl.java
+++ src/vlab/server_java/check/CheckProcessorImpl.java
@@ -9,29 +9,24 @@ import vlab.server_java.generate.Variant;
 import vlab.server_java.model.solutionchecking.MatrixAnswer;
 import vlab.server_java.model.solutionchecking.Solution;
 
-import javax.annotation.Nonnull;
 import java.math.BigDecimal;
 import java.util.Optional;
-
-import static java.math.BigDecimal.valueOf;
 
 /**
  * Simple CheckProcessor implementation. Supposed to be changed as needed to provide
  * necessary Check method support.
  */
 public class CheckProcessorImpl implements PreCheckResultAwareCheckProcessor<String> {
-    private static final double MSE_EPS = 0.01;
-    private static final double MATRIX_VALUE_EPS = 0.01;
-
-    private static final double MSE_DIFF_PENALTY_COEFFICIENT = 10;
-    private static final double MATRICES_COUNT_DIFF_PENALTY_COEFFICIENT = 20;
-    private static final double MATRICES_VALUE_DIFF_PENALTY_COEFFICIENT = 1;
+    private static final double SINGLE_VALID_MATRIX_POINTS = 7.0;
+    private static final double MSE_VALID_POINTS = 16.0;
+    private static final double COMPARISON_EPS = 0.01;
 
     @Override
     public CheckingSingleConditionResult checkSingleCondition(ConditionForChecking condition, String instructions, GeneratingResult generatingResult) {
+        // TODO: 21.04.2022  add test!
         System.out.println("instructions in checkProcessor" + instructions);
 
-        BigDecimal points = BigDecimal.valueOf(1.0);
+        double points = 0.0;
         StringBuilder commentBuilder = new StringBuilder();
 
         try {
@@ -40,84 +35,58 @@ public class CheckProcessorImpl implements PreCheckResultAwareCheckProcessor<Str
             Solution studentSolution = JacksonHelper.fromJson(instructions, Solution.class);
             Solution ourSolution = new Solution(generatedVariant);
 
-            //compare ourSolution and studentSolution
-            //todo change checking strategy to fail-fast
-            points = compareMSE(studentSolution, ourSolution, commentBuilder, points);
-            points = compareMatricesCount(studentSolution, ourSolution, commentBuilder, points);
-            points = compareMatricesValues(studentSolution, ourSolution, commentBuilder, points);
+            //compare ourSolution and studentSolution from the very beginning to the end
+            for (MatrixAnswer studentMatrix : studentSolution.matrices) {
+                Optional<MatrixAnswer> ourMatrixOptional = ourSolution.matrices
+                        .stream()
+                        .filter(ourMatrix -> ourMatrix.matrixId.equals(studentMatrix.matrixId))
+                        .findFirst();
+
+                if (ourMatrixOptional.isPresent()) {
+                    MatrixAnswer ourMatrix = ourMatrixOptional.get();
+                    double[][] studentMatrixValue = studentMatrix.matrixValue;
+                    double[][] ourMatrixValue = ourMatrix.matrixValue;
+
+                    int h = ourMatrixValue.length;
+                    int w = ourMatrixValue[0].length;
+                    if (studentMatrixValue.length != h || studentMatrixValue[0].length != w) {
+                        commentBuilder.append("Матрица ").append(ourMatrix.matrixId).append(" должна иметь размер ")
+                                .append(h).append("x").append(w).append(", но имеет размер ").append(studentMatrixValue.length).append("x").append(studentMatrixValue[0].length).append(";\n");
+                        break;
+                    } else {
+                        for (int i = 0; i < h; i++) {
+                            for (int j = 0; j < w; j++) {
+                                double diff = Math.abs(studentMatrixValue[i][j] - ourMatrixValue[i][j]);
+                                if (Double.compare(diff, COMPARISON_EPS) > 0) {
+                                    commentBuilder.append("Матрица ").append(ourMatrix.matrixId).append(", ячейка (").append(i).append(",")
+                                            .append(j).append("): ожидаемое значение - ").append(ourMatrixValue[i][j]).append(", актуальное значение - ").append(studentMatrixValue[i][j]);
+                                    break;
+                                }
+                            }
+                        }
+                        points += SINGLE_VALID_MATRIX_POINTS;
+                    }
+                } else {
+                    commentBuilder.append("qwe");
+                    break;
+                }
+            }
+
+            double mseDiff = Math.abs(studentSolution.mse - ourSolution.mse);
+            if (Double.compare(mseDiff, COMPARISON_EPS) > 0) {
+                commentBuilder.append("MSE отличается от правильного (").append(ourSolution.mse).append(") больше чем на ").append(COMPARISON_EPS).append(";\n");
+            } else {
+                points += MSE_VALID_POINTS;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return new CheckingSingleConditionResult(
-                Double.compare(points.doubleValue(), 0.0) > 0
-                        ? points : BigDecimal.valueOf(0.0),
+                Double.compare(points, 0.0) > 0
+                        ? BigDecimal.valueOf(points) : BigDecimal.valueOf(0.0),
                 commentBuilder.toString()
         );
-    }
-
-    private BigDecimal compareMatricesValues(@Nonnull Solution studentSolution,
-                                             @Nonnull Solution ourSolution,
-                                             @Nonnull StringBuilder commentBuilder,
-                                             @Nonnull BigDecimal points) {
-        for (MatrixAnswer studentMatrix : studentSolution.matrices) {
-            Optional<MatrixAnswer> ourMatrixOptional = ourSolution.matrices
-                    .stream()
-                    .filter(ourMatrix -> ourMatrix.matrixId.equals(studentMatrix.matrixId))
-                    .findFirst();
-
-            if (ourMatrixOptional.isPresent()) {
-                MatrixAnswer ourMatrix = ourMatrixOptional.get();
-                double[][] studentMatrixValue = studentMatrix.matrixValue;
-                double[][] ourMatrixValue = ourMatrix.matrixValue;
-
-                int h = ourMatrixValue.length;
-                int w = ourMatrixValue[0].length;
-                if (studentMatrixValue.length != h || studentMatrixValue[0].length != w) {
-                    commentBuilder.append("Матрица ").append(ourMatrix.matrixId).append(" должна иметь размер ")
-                            .append(h).append("x").append(w).append(", но имеет размер ").append(studentMatrixValue.length).append("x").append(studentMatrixValue[0].length).append(";\n");
-                    points = points.subtract(valueOf(0.2));
-                } else {
-                    for (int i = 0; i < h; i++) {
-                        for (int j = 0; j < w; j++) {
-                            double diff = Math.abs(studentMatrixValue[i][j] - ourMatrixValue[i][j]);
-                            if (Double.compare(diff, MATRIX_VALUE_EPS) > 0) {
-                                commentBuilder.append("Матрица ").append(ourMatrix.matrixId).append(", ячейка (").append(i).append(",")
-                                        .append(j).append("): ожидаемое значение - ").append(ourMatrixValue[i][j]).append(", актуальное значение - ").append(studentMatrixValue[i][j]);
-                                points = points.subtract(valueOf(diff * MATRICES_VALUE_DIFF_PENALTY_COEFFICIENT));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return points;
-    }
-
-    private BigDecimal compareMatricesCount(@Nonnull Solution studentSolution,
-                                            @Nonnull Solution ourSolution,
-                                            @Nonnull StringBuilder commentBuilder,
-                                            @Nonnull BigDecimal points) {
-        int size = ourSolution.matrices.size();
-        int matricesCntDiff = Math.abs(studentSolution.matrices.size() - size);
-        if (matricesCntDiff != 0) {
-            commentBuilder.append("Количество результирующих матриц отличается от правильного (").append(size).append(") на ").append(matricesCntDiff).append(";\n");
-            return points.subtract(valueOf(matricesCntDiff * MATRICES_COUNT_DIFF_PENALTY_COEFFICIENT));
-        }
-        return points;
-    }
-
-    private BigDecimal compareMSE(@Nonnull Solution studentSolution,
-                                  @Nonnull Solution ourSolution,
-                                  @Nonnull StringBuilder commentBuilder,
-                                  @Nonnull BigDecimal points) {
-        double mseDiff = Math.abs(studentSolution.mse - ourSolution.mse);
-        if (Double.compare(mseDiff, MSE_EPS) > 0) {
-            commentBuilder.append("MSE отличается от правильного (").append(ourSolution.mse).append(") больше чем на ").append(MSE_EPS).append(";\n");
-            return points.subtract(valueOf(mseDiff * MSE_DIFF_PENALTY_COEFFICIENT));
-        } else {
-            return points;
-        }
     }
 
     @Override
